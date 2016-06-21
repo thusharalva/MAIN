12ac3be3558a0b6ac946c5504a88f3679e7e20a8
thusharalva <thusharalva1990@gmail.com>
2016-06-22 00:26:47 +0530

    java changes
    changes java

---
+++ Server/askformedicine/src/main/java/com/ask/AskformedicineApplication.java
@@ -0,0 +1,12 @@
+package com.ask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskformedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskformedicineApplication.class, args);
+	}
+}
---
+++ Server/askformedicine/src/test/java/com/ask/AskformedicineApplicationTests.java
@@ -0,0 +1,18 @@
+package com.ask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AskformedicineApplication.class)
+@WebAppConfiguration
+public class AskformedicineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
