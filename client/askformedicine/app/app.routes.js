50bf0ea0c3a752c87720937e8a3b0516966af16c
Thilakraj <thilakrajkm@gmail.com>
2016-07-01 01:19:28 +0530

    Product model & bulkupload initial commit

---
+++ client/askformedicine/app/app.routes.js
@@ -0,0 +1,10 @@
+"use strict";
+var router_1 = require('@angular/router');
+// import { DashboardComponent } from './dashboard.component';
+// import { HeroesComponent } from './heroes.component';
+// import { HeroDetailComponent } from './hero-detail.component';
+exports.routes = [];
+exports.APP_ROUTER_PROVIDERS = [
+    router_1.provideRouter(exports.routes)
+];
+//# sourceMappingURL=app.routes.js.map
